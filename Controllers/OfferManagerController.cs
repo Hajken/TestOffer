97613a31ecb4c691c4cea3a57d395dd1504ee82f
johan haak <johanhaaken@gmail.com>
2016-05-10 15:40:17 +0200

    Created ItemViewModel that will handle the list of items

---
+++ Controllers/OfferManagerController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+using System.Web;
+using System.Web.Mvc;
+using TestOffer.Models;
+using TestOffer.ViewModels;
+
+namespace TestOffer.Controllers
+{
+    public class OfferManagerController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        ItemViewModel itemList = new ItemViewModel();
+
+        // GET: OfferManager
+        public ActionResult Index()
+        {
+            //Get all items from Database and add to ItemViewModel
+            itemList.Items = db.ItemModels.ToList();
+            var metalItemModels = db.MetalItemModels.Include(p => p.Item);
+            itemList.MetalItems = metalItemModels.ToList();
+
+            return View(itemList);
+        }
+
+
+        public ActionResult AddToCart()
+        {
+
+
+            return View();
+        }
+    }
+}
---
+++ ViewModels/ItemViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TestOffer.Models;
+
+namespace TestOffer.ViewModels
+{
+    public class ItemViewModel
+    {
+        public List<ItemModels> Items { get; set; }
+        public List<MetalItemModels> MetalItems { get; set; }
+
+
+    }
+}
